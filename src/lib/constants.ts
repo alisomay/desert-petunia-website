a26cabfeeb002598d10fac77e0545d164c0b6294
Ali Somay <alisomay@runbox.com>
2025-01-26 12:12:31 +0100

    Remove env vars for constant data

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,13 +1,20 @@
 // src/lib/constants.ts
 import { SiteConfig } from "@/types";
 
+// export const siteConfig: SiteConfig = {
+//   name: process.env.NEXT_PUBLIC_SITE_NAME ?? "Desert Petunia",
+//   description:
+//     process.env.NEXT_PUBLIC_SITE_DESCRIPTION ??
+//     `Preserve the "beginner's mind"`,
+//   url: process.env.NEXT_PUBLIC_SITE_URL ?? "https://desertpetunia.com",
+//   ogImage: `${
+//     process.env.NEXT_PUBLIC_SITE_URL ?? "https://desertpetunia.com"
+//   }/og.jpg`,
+// } as const;
+
 export const siteConfig: SiteConfig = {
-  name: process.env.NEXT_PUBLIC_SITE_NAME ?? "Desert Petunia",
-  description:
-    process.env.NEXT_PUBLIC_SITE_DESCRIPTION ??
-    `Preserve the "beginner's mind"`,
-  url: process.env.NEXT_PUBLIC_SITE_URL ?? "https://desertpetunia.com",
-  ogImage: `${
-    process.env.NEXT_PUBLIC_SITE_URL ?? "https://desertpetunia.com"
-  }/og.jpg`,
+  name: "Desert Petunia",
+  description: `Preserve the "beginner's mind"`,
+  url: "https://desertpetunia.com",
+  ogImage: `${"https://desertpetunia.com"}/og.jpg`,
 } as const;
